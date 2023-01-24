2efd7f002cde63988228675992e1187797d2af86
Thomas_D <t.dare@ludus-academie.com>
2023-01-24 17:58:10 +0100

    Ajoutez des fichiers projet.

---
+++ Flappy-Fraud/Game1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Flappy_Fraud
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        public SpriteBatch _spriteBatch;
+        public Bird Player = new Bird();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            Texture2D birdTexture = Content.Load<Texture2D>("Flappy-Bird-Spritesheet");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Bisque);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw();
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
