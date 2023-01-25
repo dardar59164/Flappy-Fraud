1521b31b1e0c3faa7bd31a1a1a767be288638833
Thomas_D <t.dare@ludus-academie.com>
2023-01-25 17:59:22 +0100

    Début de jeu...

--- Flappy-Fraud/Game1.cs
+++ Flappy-Fraud/Game1.cs
@@ -11,8 +11,19 @@ namespace Flappy_Fraud
         public SpriteBatch _spriteBatch;
         public Bird Player;
         Texture2D backgroundTexture;
+        Texture2D pipeTexture;
+
+        Rectangle pipeBounds = new Rectangle(
+            (int)0,
+            (int)0,
+            (int)52,
+            (int)320
+        );
+
         private Vector2 _center;
         private Vector2 _position;
+        private float pipeX;
+        private float pipeY;
         private float _moveSpeed;
         private float animationSpeed = 1 / 5f;
         private float currentTime = 0;
@@ -36,6 +47,9 @@ namespace Flappy_Fraud
             //Get the center of the screen
             _center = new Vector2((GraphicsDevice.Viewport.Width / 2), GraphicsDevice.Viewport.Height / 2);
             _position = new Vector2(_center.X - 50, _center.Y);
+
+            pipeX = GraphicsDevice.Viewport.Width;
+
             _moveSpeed = 10f;
             Player = new Bird(_position);
             //Initialize Player Animation Frame
@@ -50,6 +64,7 @@ namespace Flappy_Fraud
 
             Player.texture = Content.Load<Texture2D>("Flappy-Bird-Spritesheet");
             backgroundTexture = Content.Load<Texture2D>("background-day");
+            pipeTexture = Content.Load<Texture2D>("pipe-green");
         }
 
         protected override void Update(GameTime gameTime)
@@ -76,11 +91,10 @@ namespace Flappy_Fraud
                 }
             }
 
-            KeyboardState state = Keyboard.GetState();
             MouseState mState = Mouse.GetState();
-            //Player Position Update
 
-            if (state.IsKeyDown(Keys.Up) || mState.LeftButton == ButtonState.Pressed)
+            //Player Position Update
+            if (mState.LeftButton == ButtonState.Pressed)
             {
                 _position.Y -= _moveSpeed;
             }
@@ -89,6 +103,13 @@ namespace Flappy_Fraud
             {
                 _position.Y += 5;
             }
+
+            //Pipe Position Update
+            if (pipeX <= 0 - pipeTexture.Width)
+            {
+                pipeX = GraphicsDevice.Viewport.Width;
+            }
+            else pipeX--;
 
             base.Update(gameTime);
         }
@@ -99,6 +120,8 @@ namespace Flappy_Fraud
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(backgroundTexture,new Vector2(0,0),Color.White);
+            _spriteBatch.Draw(pipeTexture, new Vector2(pipeX, GraphicsDevice.Viewport.Height / 2), Color.White);
+            //_spriteBatch.Draw(pipeTexture, new Vector2(pipeX, GraphicsDevice.Viewport.Height / 2),pipeBounds, Color.White,0f,Vector2.Zero,SpriteEffects.FlipVertically,0);
             _spriteBatch.Draw(Player.texture, _position,Player.spriteAnimation[Player.animationFrame],Color.White);
             _spriteBatch.End();
 
